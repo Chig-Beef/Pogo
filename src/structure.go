749162b7d3e6582b070853413a29ac07947598b8
Chig Beef <rhystobyharris@gmail.com>
2024-01-20 11:08:40 +1030

    Sorted "structureCode" map

--- src/structure.go
+++ src/structure.go
@@ -13,72 +13,81 @@ func (st Structure) stringify() string {
 	for i := 0; i < len(st.children); i++ {
 		text += "\n" + st.children[i].stringify()
 	}
-	var t string
-	if st.text == "\n" {
-		t = "NEWLINE"
-	} else {
-		t = st.text
-	}
-	return t + strings.ReplaceAll(text, "\n", "\n\t")
+	return st.text + strings.ReplaceAll(text, "\n", "\n\t")
 }
 
 var structureCode map[string]int = map[string]int{
 	// Not implemented
 	"ILLEGAL": -1,
 
-	"ST_IMPORT":     0,
-	"IF_ELSE_BLOCK": 1,
-	"BLOCK":         2,
-	"ST_IF":         3,
-	"ST_ELIF":       4,
-	"ST_ELSE":       5,
-	"CALL":          6,
-	"ST_FOR":        7,
-	"EXPRESSION":    8,
-	"PROGRAM":       9,
-	"STATEMENT":     10,
-	"ASTERISK":      11,
-	"K_IMPORT":      12,
-	"K_FROM":        13,
-	"K_FOR":         14,
-	"K_IN":          15,
-	"K_IF":          16,
-	"K_ELIF":        17,
-	"K_ELSE":        18,
-	"IB_PRINT":      19,
-	"IB_RANGE":      20,
-	"BO_NOT":        21,
-	"BO_AND":        22,
-	"BO_OR":         23,
-	"MO_PLUS":       24,
-	"MO_SUB":        25,
-	"MO_MUL":        26,
-	"MO_DIV":        27,
-	"MO_MODULO":     28,
-	"IDENTIFIER":    29,
-	"NEWLINE":       30,
-	"INDENT":        31,
-	"L_PAREN":       32,
-	"R_PAREN":       33,
-	"L_BLOCK":       34,
-	"R_BLOCK":       35,
-	"L_SQUIRLY":     36,
-	"R_SQUIRLY":     37,
-	"SEP":           38,
-	"COLON":         39,
-	"ANTI_COLON":    40,
-	"ASSIGN":        41,
-	"UNDETERMINED":  42,
-	"COMMENT_ONE":   43,
-	"L_BOOL":        44,
-	"L_INT":         45,
-	"L_STRING":      46,
-	"CO_EQUALS":     47,
-	"CO_NOT_EQUALS": 48,
-	"CO_GT":         49,
-	"CO_GT_EQUALS":  50,
-	"CO_LT":         51,
-	"CO_LT_EQUALS":  52,
-	"LITERAL":       53,
-	"COMPARISON":    54,
+	// Statements
+	"ST_IMPORT":       0,
+	"IF_ELSE_BLOCK":   1,
+	"ST_IF":           2,
+	"ST_ELIF":         3,
+	"ST_ELSE":         4,
+	"ST_FOR":          5,
+	"ST_DECLARATION":  6,
+	"ST_MANIPULATION": 7,
+
+	// Other
+	"BLOCK":        32,
+	"CALL":         33,
+	"EXPRESSION":   34,
+	"COMPARISON":   35,
+	"PROGRAM":      36,
+	"ASTERISK":     37,
+	"IDENTIFIER":   38,
+	"NEWLINE":      39,
+	"INDENT":       40,
+	"L_PAREN":      41,
+	"R_PAREN":      42,
+	"L_BLOCK":      43,
+	"R_BLOCK":      44,
+	"L_SQUIRLY":    45,
+	"R_SQUIRLY":    46,
+	"SEP":          47,
+	"COLON":        48,
+	"ANTI_COLON":   49,
+	"ASSIGN":       50,
+	"UNDETERMINED": 51,
+	"COMMENT_ONE":  52,
+
+	// Keywords
+	"K_IMPORT": 64,
+	"K_FROM":   65,
+	"K_FOR":    66,
+	"K_IN":     67,
+	"K_IF":     68,
+	"K_ELIF":   69,
+	"K_ELSE":   70,
+
+	// In-built functions
+	"IB_PRINT": 96,
+	"IB_RANGE": 97,
+
+	// Bool operands
+	"BO_NOT": 128,
+	"BO_AND": 129,
+	"BO_OR":  130,
+
+	// Math operands
+	"MO_PLUS":   160,
+	"MO_SUB":    161,
+	"MO_MUL":    162,
+	"MO_DIV":    163,
+	"MO_MODULO": 164,
+
+	// Literal
+	"L_BOOL":   192,
+	"L_INT":    193,
+	"L_STRING": 194,
+
+	// Comparison operands
+	"CO_EQUALS":     224,
+	"CO_NOT_EQUALS": 225,
+	"CO_GT":         226,
+	"CO_GT_EQUALS":  227,
+	"CO_LT":         228,
+	"CO_LT_EQUALS":  229,
 }
